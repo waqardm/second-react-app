cc4a0e5eb111011f86a7ef361933df11770ad08b
Waqar Mohammad <waqar@waqar.co>
2018-12-01 22:42:51 +0000

    Day 3 completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Room extends React.Component {
+    state = {
+        isLit: true,
+        temp : 22
+   }
+    
+   flipLight = () => {
+        this.setState({
+            isLit: !this.state.isLit
+        });
+   };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+   turnOn = () => {
+       this.setState({
+           isLit: !this.state.isList
+       });
+   }
+
+    turnOff = () => {
+        if(this.state.isLit){
+            this.setState({
+                isLit: !this.state.isLit
+            })
+        }
+    }
+
+    tempUp = () => {
+        this.setState({
+            temp: this.state.temp + 1
+        })
+    }
+
+    tempDown = () => {
+        this.setState({
+            temp: this.state.temp -1
+        })
+    }
+
+
+    render() {
+       const brightness = this.state.isLit ? "lit" : "dark";
+       return (
+           <div className={ `room ${ brightness }` }>
+               the room is {this.state.isLit ? 'lit' : 'dark' }
+               <br />
+               <button onClick={ this.flipLight }>Flip</button>
+
+               <br />
+                <button onClick={ this.turnOn }>Turn On</button>
+                <button onClick={ this.turnOff }>Turn Off</button>
+
+                <br />
+                <br />
+
+                the temp is { this.state.temp }c
+                <br />
+                <button onClick={ this.tempDown }>Temp Down</button>
+                <button onClick={ this.tempUp }>Temp Up</button>
+           </div>
+       );
+    }
+}
+
+
+
+
+//------------- components are stateless, use classes instead --------//
+// function Room (){
+//     return (
+//         <div className="room">the room is lit</div>
+//     );
+// }
+
+ReactDOM.render(<Room />, document.getElementById('root'));
